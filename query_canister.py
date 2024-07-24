3a53e0a72c82bb736a4562ea47da72e284b9b7d9
Patricio Zavala Fuenzalida <pazavala@uc.cl>
2024-07-24 19:09:56 -0400

    ON IT: python script to query the deployed canister

--- query_canister.py
+++ query_canister.py
@@ -13,37 +13,55 @@ headers = {
     "Content-Type": "application/cbor",
 }
 
+# Define the body data
+body_data = {
+    "recipient": "ucuh4-xvinn-x5ac5-snla4-t65g2-5bpiw-awzag-znjhh-fzdek-cut2o-aae",
+    "amount": 11,
+}
+
+# Encode the body data correctly
+body_type = Types.Record({
+    "recipient": Types.Text,  # Convert Principal to Text
+    "amount": Types.Nat,
+})
+
+encoded_body = encode([{
+    "type": body_type,
+    "value": body_data
+}])
+
 data = {
     "method": "POST",
     "url": "/",
     "headers": [
         ("Content-Type", "application/cbor"),
     ],
-    "body": {
-        "recipient": "ucuh4-xvinn-x5ac5-snla4-t65g2-5bpiw-awzag-znjhh-fzdek-cut2o-aae",
-        "amount": 11,
-    },
+    "body": encoded_body
 }
+
+# Define the payload type
 payload_type = Types.Record({
     "method": Types.Text,
     "url": Types.Text,
-    "headers": Types.Vec(Types.Tuple(Types.Text)),
-    "body": Types.Record({
-        "recipient": Types.Principal,
-        "amount": Types.Nat,
-    })
+    "headers": Types.Vec(Types.Tuple(Types.Text, Types.Text)),
+    "body": Types.Vec(Types.Nat8),  # Representing blob
 })
+
 encoded_args = encode([{"type": payload_type, "value": data}])
+
+print("encoded args", encoded_args)
 query_payload = {
     "content": {
         "request_type": "query",
-        "sender": "7s5ng-b62ou-yb365-kgm6b-baeoa-6zfrp-k2a2d-yqud3-3txbe-qdwci-iae",
+        "sender": Principal.from_str("7s5ng-b62ou-yb365-kgm6b-baeoa-6zfrp-k2a2d-yqud3-3txbe-qdwci-iae").bytes,
         "ingress_expiry": int((time.time() + 60) * 1_000_000_000),  # 60 seconds from now in nanoseconds
-        "canister_id": canister_id,
+        "canister_id": Principal.from_str(canister_id).bytes,
         "method_name": "http_request",
         "arg": encoded_args,
     },
 }
+
+print("query_payload", query_payload)
 
 response = requests.post(
   url,

08f9b3ff8f8eaf4f985c85360f8869a198423b37
Mauro Mendoza <msmendozael@icloud.com>
2024-07-26 19:13:16 -0400

    refactor(query_canister): rename file

--- query_canister.py
+++ query_canister.py
@@ -1,118 +1,118 @@
-from ic import Principal
+import pprint
 import requests
 from ic.candid import encode, Types
 import cbor2
+from ic import Identity, Principal, decode
 import time
+from pprint import pprint
 
 
-# define the canister URL (replace with your actual canister URL)
-canister_id = "u4ezz-mqaaa-aaaan-qmrva-cai"
+# Replace with your canister ID and method name
+canister_id = "jivd6-uaaaa-aaaar-qahbq-cai"
+method_name = "http_request"
+
 url = f"https://nns.ic0.app/api/v2/canister/{canister_id}/query"
 
-
-
-
-# Prepare the query data (example data, replace with actual parameters)
-
 # Define the recipient and amount arguments
-recipient = "ucuh4-xvinn-x5ac5-snla4-t65g2-5bpiw-awzag-znjhh-fzdek-cut2o-aae"
-amount = 11
-# Prepare the query arguments as a dictionary
+recipient = Principal.from_str("f6fvu-25ywu-a2oez-2oc7d-3thap-r5d6f-uez55-ltn4b-tw4yn-fqu66-aae").bytes
+amount = 0.0000015
+
+# Prepare the query arguments as a dictionary and encode using Candid
 args = {
-    "recipient": "ucuh4-xvinn-x5ac5-snla4-t65g2-5bpiw-awzag-znjhh-fzdek-cut2o-aae",
-    "amount": 11
+    "recipient": recipient,
+    "amount": amount
 }
 
+# Define the structure of the arguments
+args_types = Types.Record({
+    "recipient": Types.Principal,
+    "amount": Types.Float64,
+})
 
-# Encode the arguments using CBOR
-# encoded_args = cbor2.dumps(args)
-encoded_args = encode(
-    [[
-        ("recipient", Types.Principal),
-        ("amount", Types.Nat)
-    ],
-    (recipient, amount)]
+# Encode the arguments using Candid
+encoded_args = encode([{"type": args_types, "value": args}])
+
+headers = {"Content-Type": "application/cbor"}
+
+# Prepare the HttpRequest structure
+http_values = {
+    "url": "/",
+    "method": "POST",
+    "headers": [
+      ("Content-Type", "application/cbor"),
+      ],
+    "body": encoded_args
+}
+
+#print("http req", http_values)
+
+http_types = Types.Record({
+  "url": Types.Text,
+  "method": Types.Text,
+  "headers": Types.Vec(Types.Tuple(Types.Text, Types.Text)),
+  "body": Types.Vec(Types.Nat8),  # Representing blob
+})
+
+# Encode the HttpRequest using Candid
+encoded_http_request = encode([{
+  "type": http_types,
+  "value": http_values
+}])
+
+# print("encoded http req", encoded_http_request)
+
+# anon sender
+identity = Identity(
+  type="ed25519",  # ed25519 | secp256k1
+  anonymous=True,
 )
 
+# Prepare the query data
 query_data = {
-    "type": "query",
-    "canister_id": canister_id,
-    "method": "http_request",
-    "arg": encoded_args
+  "content":{
+    "request_type": "query",
+    "sender": identity.sender().bytes,
+    "sender_sig": identity.sign(encoded_http_request),
+    "canister_id": Principal.from_str(canister_id).bytes,
+    "ingress_expiry": int((time.time() + 60) * 1_000_000_000),
+    "method_name": method_name,
+    "arg": encoded_http_request
+  }
 }
-
+# print("canid", cbor2.dumps(Principal.from_str(canister_id).bytes))
 # Encode the entire query data to CBOR format
 cbor_encoded_data = cbor2.dumps(query_data)
 
 # Send the query request
-response = requests.post(url, data=cbor_encoded_data)
-print(response.content)
+response = requests.post(url, headers= headers, data=cbor_encoded_data)
+print("res content\n", response.content, "\n")
 
 # Decode the response from CBOR format
 response_data = cbor2.loads(response.content)
 
 # Print the response
-print(response_data)
+print("res data\n", response_data, "\n")
 
-###########
+if response_data.get("status") == "rejected":
+  print(f"status        : {response_data['status']}")
+  print(f"error_code    : {response_data['error_code']}")
+  print(f"reject_code   : {response_data['reject_code']}")
+  print(f"reject_message: {response_data['reject_message']}")
+  print(f"signatures    : \n{pprint.pformat(response_data['signatures'][0])}")
 
-# headers = {
-#     "Content-Type": "application/cbor",
-# }
 
-# # Define the body data
-# body_data = {
-#     "recipient": "ucuh4-xvinn-x5ac5-snla4-t65g2-5bpiw-awzag-znjhh-fzdek-cut2o-aae",
-#     "amount": 11,
-# }
+if response_data.get("status") == "replied":
+  print("status\n", response_data["status"], "\n")
+  print("reply\n", response_data["reply"], "\n")
+  print("signatures\n", response_data["signatures"], "\n")
 
-# # Encode the body data correctly
-# body_type = Types.Record({
-#     "recipient": Types.Text,  # Convert Principal to Text
-#     "amount": Types.Nat,
-# })
+  print("arg")
+  arg = decode(response_data["reply"]["arg"])[0]
 
-# encoded_body = encode([{
-#     "type": body_type,
-#     "value": body_data
-# }])
+  # rename the keys
+  arg["value"]["status"] = arg["value"].pop("_3475804314")
+  arg["value"]["headers"] = arg["value"].pop("_1661489734")
+  arg["value"]["body"] = arg["value"].pop("_1092319906")
 
-# data = {
-#     "method": "POST",
-#     "url": "/",
-#     "headers": [
-#         ("Content-Type", "application/cbor"),
-#     ],
-#     "body": encoded_body
-# }
-
-# # Define the payload type
-# payload_type = Types.Record({
-#     "method": Types.Text,
-#     "url": Types.Text,
-#     "headers": Types.Vec(Types.Tuple(Types.Text, Types.Text)),
-#     "body": Types.Vec(Types.Nat8),  # Representing blob
-# })
-
-# encoded_args = encode([{"type": payload_type, "value": data}])
-
-# print("encoded args", encoded_args)
-# query_payload = {
-#     "content": {
-#         "request_type": "query",
-#         "sender": Principal.from_str("7s5ng-b62ou-yb365-kgm6b-baeoa-6zfrp-k2a2d-yqud3-3txbe-qdwci-iae").bytes,
-#         "ingress_expiry": int((time.time() + 60) * 1_000_000_000),  # 60 seconds from now in nanoseconds
-#         "canister_id": Principal.from_str(canister_id).bytes,
-#         "method_name": "http_request",
-#         "arg": encoded_args,
-#     },
-# }
-
-# print("query_payload", query_payload)
-
-# response = requests.post(
-#   url,
-#   headers=headers,
-#   data=cbor2.dumps(query_payload),
-# )
-# print(f"{response.status_code} - {response.text}")
+  arg["value"]["body"] = "".join(chr(i) for i in arg["value"]["body"])
+  pprint(arg)
